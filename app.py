03d1a177771b2bb3c3abc3e9214b5bc41fb10432
kensoidev <113433946+kensoidev@users.noreply.github.com>
2022-12-19 18:19:26 +0500

    введение лог файла

--- app.py
+++ app.py
@@ -7,6 +7,12 @@
 """
 
 import re # для разделения строки при помощи регулярных выражений
+import logging # для введения log файла
+
+logging.basicConfig(
+    filename="./log.txt",
+    level=logging.DEBUG, format="%(asctime)s [%(levelname)s]\t%(message)s", encoding="utf-8"
+    )
 
 def run_operator(first_value:int, second_value:int, operator = "+"):
     """
@@ -21,6 +27,11 @@ def run_operator(first_value:int, second_value:int, operator = "+"):
         "/": lambda x, y: x / y
     }
 
+    logging.debug(
+        "Совершено вычисление при помощи выражения %s %s %s",
+        first_value, operator, second_value
+        )
+
     return expressions[operator](first_value, second_value)
 
 
@@ -28,38 +39,73 @@ def main():
     """
     Главная функция
     """
+    logging.info(
+        "Программа начата"
+        )
     # ввод строки
     raw_expression = input("Введите выражение >> ")
 
+    logging.debug(
+        "Введено выражение : %s ", raw_expression
+        )
     # очищаем пробелы, т.к. привычка писать с пробелами :)
     while raw_expression.count(" ") != 0:
+        logging.debug(
+            "Очистка пробелов"
+            )
         raw_expression = raw_expression.replace(" ", "")
-
+    logging.debug(
+        "Очистка пробелов"
+        )
     # разделяем строку на числа при помощи регулярного выражения
     value_list = list(map(int, re.split('[-+*/]', raw_expression)))
 
+    logging.debug(
+        "числа в выражении : %s ", value_list
+        )
+    logging.debug(
+        "длина списка чисел : %s ", len(value_list)
+        )
     if len(value_list) > 3:
         raise Exception("слишком много чисел")
 
     # ищем операторы которые есть в выражении (тооочно таким же образом)
     operators_list = list(filter(lambda x: x != "", re.split(r'\d+', raw_expression)))
 
+    logging.debug(
+        "список операторов : %s ", operators_list
+        )
     # print(value_list, operators_list)
 
+    logging.info(
+        "парсинг завершён, переход к вычислениям"
+        )
     if len(operators_list) == 2:
         if operators_list[1] in ["*", "/"]:
+            logging.info(
+                "начинаем с второго оператора по приоритету"
+                )
             first_step = run_operator(value_list[1], value_list[2], operators_list[1])
             second_step = run_operator(value_list[0], first_step, operators_list[0])
 
         else:
+            logging.info(
+                "начинаем с первого оператора по приоритету"
+                )
             first_step = run_operator(value_list[0], value_list[1], operators_list[0])
             second_step = run_operator(value_list[2], first_step, operators_list[1])
 
         result = second_step
 
     else:
+        logging.info(
+            "вычисление без приоритетов"
+            )
         result = run_operator(value_list[0], value_list[1], operators_list[0])
 
+    logging.info(
+        "вывод решения в консоль"
+        )
     print("Вывод", "\t"*2, "<<", result)
     return 0
 
